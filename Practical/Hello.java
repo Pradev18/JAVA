17054d98f5b32bf96bca979120ac48874e3fdb66
Prathamesh Devkate <80754725+Pradev18@users.noreply.github.com>
2023-01-20 15:47:21 +0530

    Add files via upload

---
+++ Practical/Hello.java
@@ -0,0 +1,9 @@
+class Hello{
+
+	public static void main(String args[]){ //entry point
+		System.out.println("It's my first program"); //will print on console println is nothing a method
+
+}
+//this is end of block
+
+}
