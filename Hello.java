c537b1628420a03b11604236d600655074c9d4e1
Prathamesh Devkate <80754725+Pradev18@users.noreply.github.com>
2023-01-20 15:36:40 +0530

    Delete Hello.java

--- Hello.java
+++
@@ -1,9 +0,0 @@
-class Hello{
-
-	public static void main(String args[]){ //entry point
-		System.out.println("It's my first program"); //will print on console println is nothing a method
-
-}
-//this is end of block
-
-}
