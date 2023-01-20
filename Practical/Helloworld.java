b7f027e64236173655908aa34a1d91a072125c5e
Prathamesh Devkate <80754725+Pradev18@users.noreply.github.com>
2023-01-20 18:22:38 +0530

    Add files via upload

---
+++ Practical/Helloworld.java
@@ -0,0 +1,17 @@
+class My{
+	public static void main(){ //entry point
+		System.out.println("Hi....");
+
+	}
+}
+
+
+class Helloworld{
+
+	public static void main(String args[]){ //entry point
+		System.out.println("It's my first program"); //will print on console println is nothing a method
+My.main();
+	}
+//this is end of block
+
+}
