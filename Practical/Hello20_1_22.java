c802b0d4d6ee9071f3e8056a46f6ed4d3c675734
Prathamesh Devkate <80754725+Pradev18@users.noreply.github.com>
2023-01-20 18:14:38 +0530

    Add files via upload

---
+++ Practical/Hello20_1_22.java
@@ -0,0 +1,17 @@
+class My{
+	public static void dp(){ //display() is a method
+	System.out.println("Hi...");
+	}
+}
+
+
+class Hello20_1_22{
+
+	public static void main(String args[]){ //entry point
+		System.out.println("It's my first program"); //will print on console println is nothing a method
+	My.dp();
+
+	}
+//this is end of block
+
+}
