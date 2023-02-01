64001a4d051080039b4ace30ed337487fa94e5c8
Prathamesh Devkate <80754725+Pradev18@users.noreply.github.com>
2023-02-01 22:29:50 +0530

    Add files via upload

---
+++ My1.java
@@ -0,0 +1,13 @@
+class My1
+{
+	int a;
+	String name;
+	public void display(){
+		System.out.println(a+" "+ name);
+	}
+	public static void main(String[] args){
+		My1 b= new My1();
+		b.display();
+	}
+
+}
